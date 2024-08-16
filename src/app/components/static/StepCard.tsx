e87042eacddfc813f69935171b215835c468b0c2
akanksha <17aks11@gmail.com>
2024-08-16 17:04:23 +0530

    included images for steps

--- src/app/components/static/StepCard.tsx
+++ src/app/components/static/StepCard.tsx
@@ -1,21 +1,26 @@
 import * as React from 'react';
 
 interface CardProps {
-  image: string;
-  alt: string,
+  image1: string;
+  image2: string;
   heading: string;
   subHeading: string;
 }
 
-export default function App({ heading, subHeading, image, alt }: CardProps) {
+export default function App({ heading, subHeading, image1, image2 }: CardProps) {
   return (
-    <div className="flex items-center justify-center">
-    <img src={image} alt={alt} className="w-1/4 h-auto rounded-xl shadow-lg" />
-    <div className='border-1 w-12 border-2 border-red-700'></div>
-    <div className="bg-baby-powder shadow-lg rounded-xl overflow-hidden w-1/2 p-6">
-      <h2 className="text-xl mb-2">{heading}</h2>
-      <p className="text-gray-600">{subHeading}</p>
+    <div className="flex items-center justify-center gap-2">
+      <img src={image1} alt='big-image' className="w-1/4 h-auto rounded-xl shadow-lg" />
+      <div className='border-1 w-16 border-2 border-red-700'></div>
+      <div className="bg-baby-powder shadow-lg rounded-xl overflow-hidden w-1/2 p-6">
+        <div className="flex items-center mb-4">
+          <span className="mr-4">
+            <img src={image2} alt="little-img" className='w-12 h-auto' />
+          </span>
+          <h2 className="text-2xl">{heading}</h2>
+        </div>
+        <p className="text-gray-600">{subHeading}</p>
+      </div>
     </div>
-  </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,52 +23,36 @@ export default function Home() {
         </h1>
         <p className="text-3xl my-1">Experience the Fun of Streaming with Friends! </p>
         <h4 className='text-xl'>
-          <span className='font-extrabold underline-txt'>Dynamite </span>
+          <span className='unl'>Dynamite </span>
           lets you and your friends stream any video together, as long as one of you has a downloaded version.
         </h4>
       </section>
 
-      <section className='section-info mt-24 px-10'>
-      <div className='w-1/2 flex justify-center'>
+      <section className='section-info mt-12 px-10 flex flex-col md:flex-row'>
+        <div className='w-full md:w-1/2 flex justify-center'>
           <img src="/assets/images/Fans.gif" alt="friends" className='section-img' />
         </div>
-        <div className='w-1/2 flex flex-col gap-3 p-4'>
-          <h1 className='text-4xl'>
-            <span className='colon'>:</span>
-            Shared experience
-          </h1>
-          <h2 className='text-xl'>  Whether it's your favorite TV shows, movies, or personal videos, <span className='underline-txt'>Dynamite</span>  brings everyone together for a shared viewing experience like never before.</h2>
+        <div className='w-full md:w-1/2 flex flex-col gap-3 p-4 text-left'>
+          <small className="text-sm text-gray-500">Bringing Friends Closer Through Shared Media</small>
+          <h1 className='text-4xl font-bold '>Shared Experience</h1>
+          <p className='text-lg'>Whether it's your favorite TV shows, movies, or personal videos, <span className='unl'>Dynamite</span> brings everyone together for a shared viewing experience like never before. Enjoy real-time reactions and more!</p>
         </div>
       </section>
 
-      <section className='section-info mt-24 px-10'>
-      <div className='flex-1 flex justify-center'>
-          <img src="/assets/images/connect.gif" alt="friends" className='section-img' />
+      <section className='section-info mt-10 px-10 flex flex-col md:flex-row'>
+        <div className='w-full md:w-1/2 flex flex-col gap-3 p-4 text-right'>
+          <small className="text-sm text-gray-500">Sync Up and Enjoy Seamless Viewing Together</small>
+          <h1 className='text-4xl font-bold'>Synchronized Playback</h1>
+          <p className='text-lg'>Experience synchronized video playback and enjoy group chat, making every movie night a <span className='unl'>blast.</span> Watch every moment unfold together with seamless streaming.</p>
         </div>
-        <div className='flex-1 flex flex-col gap-3 p-4'>
-          <h1 className='text-4xl'>
-            <span className='colon'>:</span>
-            Synchronized video playback
-          </h1>
-          <h2 className='text-xl'>  Experience synchronized video playback and enjoy group chat, making every movie night a <span className='underline-txt'>blast.</span> </h2>
+        <div className='w-full md:w-1/2 flex justify-center'>
+          <img src="/assets/images/connect.gif" alt="friends" className='section-img' />
         </div>
       </section>
 
-      {/* <section className='flex mt-24'>
-        <div className='flex-1 flex justify-center'>
-          <img src="/assets/images/connect.gif" alt="friends" className='section-img' />
-        </div>
-        <div className='flex-1 flex flex-col gap-3 p-4'>
-          <h1 className='text-4xl'>
-            <span className='colon'>:</span>
-            Synchronized video playback
-          </h1>
-          <h2 className='text-xl'> Experience synchronized video playback and enjoy group chat, making every movie night a <span className='underline-txt'>blast. </span> </h2>
-        </div>
-      </section> */}
 
-      <section className='bg-gradient-to-r from-[#f9ecec] md:to-[#f1f0f4] p-24 my-6 w-full scroll-smooth' id='guideSection'>
-        <h1 className='text-4xl text-center'>How does <span className='underline-txt font-extrabold'>Dynamite</span> work?</h1>
+      <section className='bg-[#F5F5F5] p-24 w-full scroll-smooth' id='guideSection'>
+        <h1 className='text-4xl text-center'>How does <span className='unl font-extrabold'>Dynamite</span> work?</h1>
         <div className="flex justify-center space-x-4 my-8">
           <button
             onClick={() => setActiveStep(1)}
@@ -97,16 +81,16 @@ export default function Home() {
         </div>
         <div className={` ${activeStep === 1 ? '' : 'hidden'}`}>
           <StepCard
-            image='/assets/images/joinIn.gif'
-            alt=''
+            image1='/assets/images/joinIn.gif'
+            image2='/assets/images/panda.png'
             heading='Get Started on Dynamite'
             subHeading='Create an account or log in to access all the features of Dynamite. Enjoy seamless streaming and sharing experiences.'
           />
         </div>
         <div className={` ${activeStep === 2 ? '' : 'hidden'}`}>
           <StepCard
-            image='/assets/images/manage.gif'
-            alt=''
+            image1='/assets/images/manage.gif'
+            image2='/assets/images/customize.png'
             heading='Manage Your Collection'
             subHeading='Upload your favorite videos to your profile. View and organize them easily, ready to stream anytime.'
           />
@@ -114,8 +98,8 @@ export default function Home() {
         </div>
         <div className={` ${activeStep === 3 ? '' : 'hidden'}`}>
           <StepCard
-            image='/assets/images/invite.gif'
-            alt=''
+            image1='/assets/images/joinIn.gif'
+            image2='/assets/images/invite.png'
             heading='Invite Your Friends'
             subHeading='Generate a unique code for your watch party and share it with your friends. They can join the fun with just a click.'
           />
@@ -123,14 +107,30 @@ export default function Home() {
         <div className={` ${activeStep === 4 ? '' : 'hidden'}`}
         >
           <StepCard
-            image='/assets/images/blast.gif'
-            alt=''
+            image1='/assets/images/blast.gif'
+            image2='/assets/images/enjoy.png'
             heading='Have a Blast'
             subHeading='Stream your videos in sync with your friends and enjoy a shared viewing experience. Chat and react in real-time for a fun-filled movie night.'
           />
 
         </div >
       </section>
+
+      <section className="bg-gradient-to-r from-[#f9ecec] md:to-[#f1f0f4] w-full py-24 flex flex-col items-center text-center">
+        <h1 className="text-4xl font-bold mb-6">
+          <span className="text-3xl mb-2">Stream.</span>
+          <span className="text-3xl mb-2"> Connect.</span>
+          <span className="text-3xl"> Celebrate.</span>
+        </h1>
+
+        <p className="mt-6 text-lg text-[#2E2E3A] font-medium">
+          Ready to dive into unforgettable movie nights? Click below to start your adventure!
+        </p>
+        <button className="bg-gray-100 text-[#2E2E3A] font-semibold py-2 px-6 rounded-md shadow-lg hover:bg-gray-200 transition-colors mt-8">
+          Get Started
+        </button>
+      </section>
+
     </main>
   );
 }
